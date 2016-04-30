212627ec36dccaafba720c74ab7b421f3227a06b
johnlawver <johnlawver@gmail.com>
2016-04-30 14:56:16 -0700

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,49 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  x= deal_card + deal_card
+  display_card_total(x)
+  return x
 end
 
-def hit?
-  # code hit? here
+def hit?(twoCardTotal)
+  prompt_user
+  input = get_user_input
+  if input == 'h' 
+    threeCardTotal = twoCardTotal + deal_card
+    return threeCardTotal
+  elsif input == 's' 
+    return twoCardTotal
+  else 
+    invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
@@ -39,6 +51,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  running_total  = initial_round
+  until running_total > 21 do 
+
+    running_total = hit?(running_total)
+    display_card_total(running_total)
+  end
+  end_game(running_total)
+  
+
 end
     
